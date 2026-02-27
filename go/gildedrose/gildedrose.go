293420bc0667a284f740ac4eeb12b5d0cdf8fa62
nakao keisuke <r3grashopper193@gmail.com>
2026-02-28 07:50:47 +0900

    refactor:ネストを削除するため早期リターンへ変更

--- go/gildedrose/gildedrose.go
+++ go/gildedrose/gildedrose.go
@@ -39,18 +39,20 @@ func UpdateQuality(items []*Item) {
 
 		items[i].SellIn = items[i].SellIn - 1
 
-		if items[i].SellIn < 0 {
-			switch items[i].Name {
-			case "Aged Brie":
-				if items[i].Quality < 50 {
-					items[i].Quality = items[i].Quality + 1
-				}
-			case "Backstage passes to a TAFKAL80ETC concert":
-				items[i].Quality = items[i].Quality - items[i].Quality
-			default:
-				if items[i].Quality > 0 {
-					items[i].Quality = items[i].Quality - 1
-				}
+		if items[i].SellIn >= 0 {
+			continue
+		}
+
+		switch items[i].Name {
+		case "Aged Brie":
+			if items[i].Quality < 50 {
+				items[i].Quality = items[i].Quality + 1
+			}
+		case "Backstage passes to a TAFKAL80ETC concert":
+			items[i].Quality = items[i].Quality - items[i].Quality
+		default:
+			if items[i].Quality > 0 {
+				items[i].Quality = items[i].Quality - 1
 			}
 		}
 	}
